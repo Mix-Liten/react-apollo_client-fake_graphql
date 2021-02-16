383f28495fefe2905a07c01964ed0b707bb02576
Mix-Liten <mixliten@gmail.com>
2021-02-16 12:10:10 +0800

    create post apolloClient + gql + useMutation

--- src/components/posts/form/postsForm.component.tsx
+++ src/components/posts/form/postsForm.component.tsx
@@ -1,9 +1,39 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+import { useCreatePost } from "../../../hooks/posts/useCreatePost";
 
 import "./postsForm.component.css";
 
+interface FormData {
+  title: string;
+  body: string;
+}
+
 const PostsForm: React.FC = () => {
-  return <div className="posts-form"></div>;
+  const { register, handleSubmit } = useForm<FormData>();
+  const createPost = useCreatePost();
+
+  const onSubmit = handleSubmit(({ title, body }) => {
+    // console.log("title & body:", { title, body });
+    createPost({
+      variables: {
+        input: {
+          title,
+          body,
+        },
+      },
+    });
+  });
+
+  return (
+    <div className="posts-form">
+      <form onSubmit={onSubmit}>
+        <input type="text" name="title" ref={register} />
+        <textarea name="body" ref={register} />
+        <input type="submit" />
+      </form>
+    </div>
+  );
 };
 
 export default PostsForm;
